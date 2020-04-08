2fff1c13e6d7eff301c0862baa29ddbdacc7edfb
KAN-RYU <kanryu2048@unist.ac.kr>
2020-04-09 05:11:29 +0900

    Boost speed

--- Text2Ebook.py
+++ Text2Ebook.py
@@ -1,5 +1,5 @@
 from PIL import ImageFont, Image, ImageDraw
-import curses
+import sys
 
 #Global Variable
 font_Name = "KoPubWorld Dotum_Pro Medium.otf"
@@ -17,6 +17,7 @@ margin = 40
 width_Limit = image_Size[0] - margin * 2
 height_Limit = image_Size[1] - margin * 2
 line_Space = font_Size // 2 #Change
+min_Line_Character = 17
 
 def insertString(origin, that, index):
     return origin[:index] + that + origin[index:]
@@ -27,24 +28,20 @@ def makePages():
     start = 0
     end = 1
     current_Page = 1
+    page_Queue = []
+    page = Image.new("RGB", image_Size)
+    page_draw = ImageDraw.Draw(page)
 
     while(start < len(input_Text)):
-        page = Image.new("RGB", image_Size)
-        page_draw = ImageDraw.Draw(page)
-        page_draw.rectangle([(0,0), image_Size], fill = page_Color)
-
+        sys.stdout.write("\r" + str(current_Page) + " Pages.")
+        sys.stdout.flush()
         while(True):
             w, h = page_draw.textsize(input_Text[start:end], font = font_Object, spacing = line_Space)
-            
+
             if h > height_Limit or end > len(input_Text):
                 #PageEnd
                 end -= 2
-                page_draw.text((margin, margin), 
-                            text = input_Text[start:end], 
-                            font = font_Object, 
-                            spacing = line_Space, 
-                            fill = font_Color, 
-                            align = "left")
+                page_Queue.append((start, end))
                 start = end + 1
                 end = start + 1
                 if not end > len(input_Text):
@@ -52,15 +49,26 @@ def makePages():
                         start += 1
                         end += 1
                 break
-            
             if w > width_Limit:
                 input_Text = insertString(input_Text, "\n", end - 1)
-                end += 2
+                end += min_Line_Character
             else:
                 end += 1
-            
-        page.save(result_Directory + "test/" + str(current_Page) + ".png", "PNG")
         current_Page += 1
+        
+    current_Page = 1
+    print()
+    for i, pair in enumerate(page_Queue):
+        sys.stdout.write("\r" + str(i + 1) + "/" + str(len(page_Queue)))
+        sys.stdout.flush()
+        page_draw.rectangle([(0,0), image_Size], fill = page_Color)
+        page_draw.text((margin, margin), 
+                            text = input_Text[pair[0]:pair[1]], 
+                            font = font_Object, 
+                            spacing = line_Space, 
+                            fill = font_Color, 
+                            align = "left")
+        page.save(result_Directory + "test/" + str(i + 1) + ".png", "PNG")
 
 
 if __name__ == "__main__":
