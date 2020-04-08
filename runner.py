cbc85799e460f1ad2fd689f6315bda1b0431a059
KAN-RYU <kanryu2048@unist.ac.kr>
2020-04-09 06:40:03 +0900

    Edit Runner.py

--- runner.py
+++ runner.py
@@ -1,2 +1,14 @@
-import pillow
-import os
+import Text2Ebook
+
+VERBOS = True
+
+def makeBooks():
+    folder_List = os.listdir(Text2Ebook.text_Directory)
+    print("Start processing total " + str(len(folder_List)) + " Books.")
+    for name in folder_List:
+        Text2Ebook.makeBook(name)
+    print("Done!")
+
+
+if __name__ == "__main__":
+    makeBooks()
