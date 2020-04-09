5b9193c4494c6b25c98d323bd2ce1c1ad6a7ae26
KAN-RYU <kanryu2048@unist.ac.kr>
2020-04-09 15:14:53 +0900

    Fix error. file name with '.'

--- Text2Ebook.py
+++ Text2Ebook.py
@@ -20,6 +20,7 @@ height_Limit = image_Size[1] - margin * 2
 line_Space = font_Size // 2 #Change
 
 verbos_line = ""
+verbos_Length = 0
 
 def insertString(origin, that, index):
     return origin[:index] + that + origin[index:]
@@ -33,47 +34,57 @@ def makePages(fileName, folderName):
     page_Queue = []
     page = Image.new("RGB", image_Size)
     page_draw = ImageDraw.Draw(page)
-    fileNameRaw = fileName[:-5]
+    global verbos_Length
+    fileNameRaw = fileName[:-4]
+    while fileNameRaw.endswith(' '):
+        fileNameRaw = fileNameRaw[:-1]
+    fileNameRaw = fileNameRaw.replace(".", "．")
 
-    while(end < len(input_Text)):
-        sys.stdout.write(verbos_line + fileNameRaw + " " + str(current_Page) + " Pages.")
+    try:
+        os.mkdir(result_Directory + folderName + fileNameRaw)
+        while(end < len(input_Text)):
+            sys.stdout.write("\r" + " " * verbos_Length)
+            verbos_M = verbos_line + fileNameRaw + " " + str(current_Page) + " Pages."
+            sys.stdout.write(verbos_M)
+            sys.stdout.flush()
+            verbos_Length = len(verbos_M.encode())
+            while(True):
+                w, h = page_draw.textsize(input_Text[start:end], font = font_Object, spacing = line_Space)
+                if h > height_Limit or end > len(input_Text):
+                    #PageEnd
+                    end -= 1
+                    page_Queue.append((start, end))
+                    start = end + 1
+                    end = start + 2
+                    if not end > len(input_Text):
+                        while(input_Text[start] == '\n' and end < len(input_Text)):
+                            start += 1
+                            end += 1
+                    break
+                if w > width_Limit:
+                    input_Text = insertString(input_Text, "\n", end - 1)
+                else:
+                    end += 1
+            current_Page += 1
+            
+        current_Page = 1
+        for i, pair in enumerate(page_Queue):
+            page_draw.rectangle([(0,0), image_Size], fill = page_Color)
+            page_draw.text((margin, margin), 
+                                text = input_Text[pair[0]:pair[1]], 
+                                font = font_Object, 
+                                spacing = line_Space, 
+                                fill = font_Color, 
+                                align = "left")
+
+            page.save(result_Directory + folderName + fileNameRaw + '/' + str(i + 1) + ".png", "PNG")
+    except FileExistsError:
+        sys.stdout.write("\r" + " " * verbos_Length)
+        verbos_M = verbos_line + fileNameRaw + " Skip."
+        sys.stdout.write(verbos_M)
         sys.stdout.flush()
-        while(True):
-            w, h = page_draw.textsize(input_Text[start:end], font = font_Object, spacing = line_Space)
-            if h > height_Limit or end > len(input_Text):
-                #PageEnd
-                end -= 1
-                page_Queue.append((start, end))
-                start = end + 1
-                end = start + 2
-                if not end > len(input_Text):
-                    while(input_Text[start] == '\n' and end < len(input_Text)):
-                        start += 1
-                        end += 1
-                break
-            if w > width_Limit:
-                input_Text = insertString(input_Text, "\n", end - 1)
-            else:
-                end += 1
-        current_Page += 1
-        
-    current_Page = 1
-    for i, pair in enumerate(page_Queue):
-        page_draw.rectangle([(0,0), image_Size], fill = page_Color)
-        page_draw.text((margin, margin), 
-                            text = input_Text[pair[0]:pair[1]], 
-                            font = font_Object, 
-                            spacing = line_Space, 
-                            fill = font_Color, 
-                            align = "left")
+        verbos_Length = len(verbos_M.encode())
 
-        try:
-            os.mkdir(result_Directory + folderName + fileNameRaw)
-        except FileExistsError:
-            pass
-
-        page.save(result_Directory + folderName + fileNameRaw + '/' + str(i + 1) + ".png", "PNG")
-    
     input_Text_File.close()
 
 def makeBook(folderName):
