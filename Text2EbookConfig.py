9bf384a926eff73764f9f7d44f5de6c3455061c9
KAN-RYU <kanryu2048@unist.ac.kr>
2020-05-03 17:04:05 +0900

    Use ConfigParser

---
+++ Text2EbookConfig.py
@@ -0,0 +1,118 @@
+import configparser
+
+class Text2EbookConfig():
+    configFilePath = './config.ini'
+    
+    #default config
+    
+    #font
+    font_Name = "KoPubWorld Dotum_Pro Medium.otf"
+    font_Size = 36
+    
+    #page
+    page_Width = 750
+    page_Height = 1060
+    margin = 40
+    min_Character = 22
+    
+    #color
+    font_Color = '#000000'
+    page_Color = '#FFFFFF'
+    
+    #directory
+    text_Directory = './Processing/'
+    result_Directory = './Result/'
+    
+    #Debug
+    verbos = False
+    
+    def __init__(self):
+        self.loadConfig()
+        
+    def loadConfig(self):
+        print('Reading', self.configFilePath, '...')
+        
+        config = configparser.RawConfigParser()
+        
+        try:
+            with open(self.configFilePath, 'r') as configFile:
+                config.read_file(configFile)
+                
+                self.font_Name = config.get('Font', 'font_Name')
+                self.font_Size = config.getint('Font', 'font_Size')
+                            
+                self.page_Width = config.getint('Page', 'page_Width')
+                self.page_Height = config.getint('Page', 'page_Height')
+                self.margin = config.getint('Page', 'margin')
+                self.min_Character = config.getint('Page', 'min_Character')
+                
+                self.font_Color = config.get('Color', 'font_Color')
+                self.page_Color = config.get('Color', 'page_Color')
+                
+                self.text_Directory = config.get('Directory', 'text_Directory')
+                self.result_Directory = config.get('Directory', 'result_Directory')
+                
+                try:
+                    self.verbos = config.getboolean('Debug', 'verbos')
+                except ValueError:
+                    self.verbos = False
+                    print("verbos = False")
+            
+        except BaseException:
+            print('Fail to load config file. Fixing config file...')
+            self.writeConfig()
+            
+        print('Reading done.')
+    
+    def writeConfig(self):
+        print('Writing config file...')
+        config = configparser.RawConfigParser()
+        
+        config.add_section('Font')
+        config.set('Font', 'font_Name', self.font_Name)
+        config.set('Font', 'font_Size', self.font_Size)
+        
+        config.add_section('Page')
+        config.set('Page', 'page_Width', self.page_Width)
+        config.set('Page', 'page_Height', self.page_Height)
+        config.set('Page', 'margin', self.margin)
+        config.set('Page', 'min_Character', self.min_Character)
+        
+        config.add_section('Color')
+        config.set('Color', 'font_Color', self.font_Color)
+        config.set('Color', 'page_Color', self.page_Color)
+        
+        config.add_section('Directory')
+        config.set('Directory', 'text_Directory', self.text_Directory)
+        config.set('Directory', 'result_Directory', self.result_Directory)
+        
+        config.add_section('Debug')
+        config.set('Debug', 'verbos', self.verbos)
+        
+        try:
+            with open(self.configFilePath, 'w') as configFile:
+                config.write(configFile)
+                configFile.close()
+        except:
+            raise
+        
+        print('Writing done.')
+    
+    def printConfig(self):
+        print('\n')
+        print('[Font]')
+        print('-font_name =', self.font_Name)
+        print('-font_size =', self.font_Size)
+        print('[Page]')
+        print('-page_width =', self.page_Width)
+        print('-page_height =', self.page_Height)
+        print('-margin =', self.margin)
+        print('-min_character', self.min_Character)
+        print('[Color]')
+        print('-font_color =', self.font_Color)
+        print('-page_color =', self.page_Color)
+        print('[Directory]')
+        print('-text_directory =', self.text_Directory)
+        print('-result_driectory =', self.result_Directory)
+        print('[Debug]')
+        print('-verbos =', self.verbos)
--- runner.py
+++ runner.py
@@ -1,7 +1,12 @@
 import Text2Ebook
+from Text2EbookConfig import Text2EbookConfig
 import os
+import sys
 
-VERBOS = True
+__config = Text2EbookConfig()
+
+VERBOS = __config.verbos
+VERSION = 'ver 0.3'
 
 def makeBooks():
     folder_List = os.listdir(Text2Ebook.text_Directory)
@@ -10,6 +15,34 @@ def makeBooks():
         Text2Ebook.makeBook(name)
     print("Done!")
 
+def menu():
+    print('\n\nText 2 Ebook Page', VERSION)
+    print('---------------------------')
+    print('1: Process without already processed file. (Noticed by existing folder)')
+    print('2: Process in working directory')
+    print('3: Process with one folder')
+    print('---------------------------')
+    print('r: Reload Config File')
+    print('p: Print Config File')
+    print('x: Exit Program')
+    print('---------------------------')
 
 if __name__ == "__main__":
-    makeBooks()
+    __config.printConfig()
+    while True:
+        menu()
+        choice = input('Type: ')
+        if choice == '1':
+            pass
+        elif choice == '2':
+            pass
+        elif choice == '3':
+            pass
+        elif choice == 'r':
+            __config.loadConfig()
+            __config.printConfig()
+        elif choice == 'p':
+            __config.printConfig()
+        elif choice == 'x':
+            sys.exit()
+    
