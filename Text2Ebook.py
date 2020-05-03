282e25d257f61ebe920690eebc8ba0b953682955
KAN-RYU <kanryu2048@unist.ac.kr>
2020-05-03 18:39:59 +0900

    with multi core processing

--- Text2Ebook.py
+++ Text2Ebook.py
@@ -4,12 +4,12 @@ import configparser
 import sys
 import os
 import parmap
+import multiprocessing
 
 class Text2Ebook():
     def __init__(self, __config):
         self.font_Name = __config.font_Name
         self.font_Size = __config.font_Size
-        self.font_Object = ImageFont.truetype(font = self.font_Name, size = self.font_Size)
         self.font_Offset = self.font_Size // 12
 
         self.font_Color = __config.font_Color
@@ -29,7 +29,8 @@ class Text2Ebook():
         self.verbos_line = ""
         self.verbos_Length = 0
 
-    def makePages(self, fileName, folderName, skipProcessed = True):
+    def makePages(self, fileName, folderName):
+        font_Object = ImageFont.truetype(font = self.font_Name, size = self.font_Size)
         input_Text_File = open(self.text_Directory +  folderName + fileName, "r", encoding = 'utf-8')
         input_Text = input_Text_File.read()
         start = 0
@@ -47,68 +48,62 @@ class Text2Ebook():
 
         nLFlag = False
 
-        sys.stdout.write("\r" + " " * self.verbos_Length)
-        verbos_M = self.verbos_line + fileNameRaw + " "
-        sys.stdout.write(verbos_M)
-        sys.stdout.flush()
+        # sys.stdout.write("\r" + " " * self.verbos_Length)
+        # verbos_M = self.verbos_line + fileNameRaw + " "
+        # sys.stdout.write(verbos_M)
+        # sys.stdout.flush()
 
-        if not os.path.exists(self.result_Directory + folderName + fileNameRaw) or not skipProcessed:
-            while(end < len(input_Text)):
-                w, h = self.font_Object.getsize_multiline(input_Text[start:end], spacing = self.line_Space)
-                if w > self.width_Limit:
-                    sys.stdout.write("\b" + spin_Bar[spin_Index])
-                    sys.stdout.flush()
-                    spin_Index = (spin_Index + 1) % 4
-                    input_Text = insertString(input_Text, "\n", end - 1)
-                    start = end
-                    end += self.min_Character
-                else:
-                    end += 1
-
-            start = 0
-            end = 1
-            cnt = 0
-            while(end < len(input_Text)):
-                if input_Text[end] == '\n':
-                    cnt += 1
-                    if(cnt == self.max_Line):
-                        page_Queue.append((start, end))
-                        start = end + 1
-                        end = start
-                        if(start < len(input_Text)):
-                            cnt = 1 if input_Text[start] == '\n' else 0
-                        current_Page += 1
+        while(end < len(input_Text)):
+            w, h = font_Object.getsize_multiline(input_Text[start:end], spacing = self.line_Space)
+            if w > self.width_Limit:
+                # sys.stdout.write("\b" + spin_Bar[spin_Index])
+                # sys.stdout.flush()
+                # spin_Index = (spin_Index + 1) % 4
+                input_Text = insertString(input_Text, "\n", end - 1)
+                start = end
+                end += self.min_Character
+            else:
                 end += 1
-            page_Queue.append((start, end))
 
-            try:
-                os.mkdir(self.result_Directory + folderName + fileNameRaw)
-            except FileExistsError:
-                pass
+        start = 0
+        end = 1
+        cnt = 0
+        while(end < len(input_Text)):
+            if input_Text[end] == '\n':
+                cnt += 1
+                if(cnt == self.max_Line):
+                    page_Queue.append((start, end))
+                    start = end + 1
+                    end = start
+                    if(start < len(input_Text)):
+                        cnt = 1 if input_Text[start] == '\n' else 0
+                    current_Page += 1
+            end += 1
+        page_Queue.append((start, end))
 
-            for i, pair in enumerate(page_Queue):
-                sys.stdout.write("\r" + " " * self.verbos_Length)
-                verbos_M = self.verbos_line + fileNameRaw + " " + str(i+1) + "/" + str(current_Page) + " Pages. "
-                sys.stdout.write(verbos_M)
-                sys.stdout.flush()
-                self.verbos_Length = min(170, len(verbos_M.encode()))
-                page_draw.rectangle([(0,0), self.image_Size], fill = self.page_Color)
-                page_draw.text((self.margin, self.margin), 
-                                    text = input_Text[pair[0]:pair[1]], 
-                                    font = self.font_Object, 
-                                    spacing = self.line_Space, 
-                                    fill = self.font_Color, 
-                                    align = "left")
+        try:
+            os.mkdir(self.result_Directory + folderName + fileNameRaw)
+        except FileExistsError:
+            pass
 
-                page.save(self.result_Directory + folderName + fileNameRaw + '/' + str(i + 1) + ".png", "PNG")
-        else:
-            sys.stdout.write("\r" + " " * self.verbos_Length)
-            verbos_M = self.verbos_line + fileNameRaw + " Skip. "
-            sys.stdout.write(verbos_M)
-            sys.stdout.flush()
-            self.verbos_Length = min(170, len(verbos_M.encode()))
+        for i, pair in enumerate(page_Queue):
+            # sys.stdout.write("\r" + " " * self.verbos_Length)
+            # verbos_M = self.verbos_line + fileNameRaw + " " + str(i+1) + "/" + str(current_Page) + " Pages. "
+            # sys.stdout.write(verbos_M)
+            # sys.stdout.flush()
+            # self.verbos_Length = min(170, len(verbos_M.encode()))
+            page_draw.rectangle([(0,0), self.image_Size], fill = self.page_Color)
+            page_draw.text((self.margin, self.margin), 
+                                text = input_Text[pair[0]:pair[1]], 
+                                font = font_Object, 
+                                spacing = self.line_Space, 
+                                fill = self.font_Color, 
+                                align = "left")
+
+            page.save(self.result_Directory + folderName + fileNameRaw + '/' + str(i + 1) + ".png", "PNG")
 
         input_Text_File.close()
+        return 1
 
     def makeBook(self, folderName, skipProcessed = True):
         if(folderName[-1] != '/'):
@@ -121,8 +116,23 @@ class Text2Ebook():
         except FileExistsError:
             pass
 
+        file_List_tmp = []
+        if skipProcessed:
+            for name in file_List:
+                fileNameRaw = name[:-4]
+                while fileNameRaw.endswith(' '):
+                    fileNameRaw = fileNameRaw[:-1]
+                fileNameRaw = fileNameRaw.replace(".", "．")
+
+                if not os.path.exists(self.result_Directory + folderName + fileNameRaw):
+                    file_List_tmp.append(name)
+            file_List = file_List_tmp
         
-        for i, name in enumerate(file_List):
-            self.verbos_line = "\r" + folderName[:-1] + " " + str(i + 1) + "/" + str(len(file_List)) + " Total Chapters. "
-            self.makePages(name, folderName, skipProcessed)
+        print(folderName[:-1], str(len(file_List)), 'chapters.')
+        num_cores = multiprocessing.cpu_count()
+        parmap.map(self.makePages, file_List, folderName, pm_pbar=True, pm_processes=num_cores)
+        
+        # for i, name in enumerate(file_List):
+        #     # self.verbos_line = "\r" + folderName[:-1] + " " + str(i + 1) + "/" + str(len(file_List)) + " Total Chapters. "
+        #     self.makePages(name, folderName)
         print()
--- runner.py
+++ runner.py
@@ -3,13 +3,6 @@ from Text2EbookConfig import Text2EbookConfig
 import os
 import sys
 
-__config = Text2EbookConfig()
-
-VERBOS = __config.verbos
-VERSION = 'ver 0.3'
-
-maker = Text2Ebook(__config)
-
 def makeBooks(skipProcessed = True):
     folder_List = os.listdir(__config.text_Directory)
     print("Start processing total " + str(len(folder_List)) + " Books.")
@@ -30,6 +23,13 @@ def menu():
     print('---------------------------')
 
 if __name__ == "__main__":
+    __config = Text2EbookConfig()
+
+    VERBOS = __config.verbos
+    VERSION = 'ver 0.3'
+
+    maker = Text2Ebook(__config)
+    
     __config.printConfig()
     while True:
         menu()
@@ -39,7 +39,8 @@ if __name__ == "__main__":
         elif choice == '2':
             makeBooks(False)
         elif choice == '3':
-            pass
+            name = input()
+            maker.makeBook(name, False)
         elif choice == 'r':
             __config.loadConfig()
             __config.printConfig()
